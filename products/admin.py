fe36b12e4b1a09289ab3d0775951813e05d96a65
MarcosLopez7 <marcos.lopezmendoza@gmail.com>
2016-05-04 20:14:07 -0500

    configurando el administrador de django

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Categoria)
+admin.site.register(Direccion)
+admin.site.register(Producto)
+admin.site.register(Flete)
+admin.site.register(Compra)
