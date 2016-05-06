d4a3d3a821c443e410ab924859877113cd362201
MarcosLopez7 <marcos.lopezmendoza@gmail.com>
2016-05-06 13:35:50 -0500

    En la misma prueba 3.0

--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import HttpResponse
+from django.views.decorators.csrf import csrf_protect
 
 # Create your views here.
+@csrf_protect
 def login(request):
 
     if request.method == 'POST':
