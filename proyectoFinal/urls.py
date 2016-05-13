efa4aeeb31533d6e7a82e1130e5253c8bbdc67d6
MarcosLopez7 <marcos.lopezmendoza@gmail.com>
2016-05-13 10:30:08 -0500

    probando rest_framework

--- proyectoFinal/urls.py
+++ proyectoFinal/urls.py
@@ -15,6 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from rest_framework import routers
+from products import views
+
+router = routers.DefaultRouter()
+router.register(r'usuario', views.UsuarioViewSet)
 
 from . import views
 
